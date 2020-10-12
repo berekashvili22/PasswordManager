acb4b3ad6b428d660c200ce701d12296eff35786
tornike <tornike.berekashvili.1@btu.edu.ge>
2020-10-12 17:23:32 +0400

    added validations to forms

--- passwordmanager/forms.py
+++ passwordmanager/forms.py
@@ -1,25 +1,44 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField, DateField, validators, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from passwordmanager.models import User
+
+
 
 class RegistrationForm(FlaskForm):
     first_name = StringField('სახელი',
-                                validators=[DataRequired(), Length(min=2, max=20)])
+                                validators=[DataRequired(message='ველის შევსება სავალდებულოა'), Length(min=2, max=20, message='ველის სიგრძე უნდა იყოს 2-20 სიმბოლომდე სიგრძის')])
     last_name = StringField('გვარი',
-                                validators=[DataRequired(), Length(min=4, max=25)])
+                                validators=[DataRequired(message='ველის შევსება სავალდებულოა'), Length(min=4, max=25, message='ველის სიგრძე უნდა იყოს 2-25 სიმბოლომდე სიგრძის')])
     email = StringField('ელ-ფოსტა',
-                                validators=[DataRequired(), Email()])
+                                validators=[DataRequired(message='ველის შევსება სავალდებულოა'), Email()])
     password = PasswordField('პაროლი',
-                                validators=[DataRequired(),])
+                                validators=[DataRequired(message='ველის შევსება სავალდებულოა'),])
     confirm_password = PasswordField('გაიმეორეთ პაროლი',
-                                validators=[DataRequired(), EqualTo('password')])
+                                validators=[DataRequired(message='ველის შევსება სავალდებულოა'), EqualTo('password', message='ველი უნდა ემთხვეოდეს თქვენს პაროლს')])
     submit = SubmitField('რეგისტრაცია')
+
+    def validate_email(self, email):
+            if User:
+                user = User.query.filter_by(email=email.data).first()
+                if user:
+                    raise ValidationError('მსგავსი ელ-ფოსტა უკვე გამოყენებულია')
+            
+            
+    class Meta:
+        def render_field(self, field, render_kw):
+            render_kw.setdefault('required', False)
+            return super().render_field(field, render_kw)  
 
 class LoginForm(FlaskForm):
     email = StringField('ელ-ფოსტა',
-                                validators=[DataRequired(), Email()])
+                                validators=[DataRequired(message='ველის შევსება სავალდებულოა'), Email()])
     password = PasswordField('პაროლი',
-                                validators=[DataRequired(),])
+                                validators=[DataRequired(message='ველის შევსება სავალდებულოა'),])
     remember = BooleanField('დამახსოვრება')
     submit = SubmitField('შესვლა')
 
+    class Meta:
+            def render_field(self, field, render_kw):
+                render_kw.setdefault('required', False)
+                return super().render_field(field, render_kw)
