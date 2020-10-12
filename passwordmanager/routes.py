fbdc85bfcf97a12c2a93d94be987a92e9316dcab
tornike <tornike.berekashvili.1@btu.edu.ge>
2020-10-12 17:22:52 +0400

    created routes

--- passwordmanager/routes.py
+++ passwordmanager/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, flash, redirect
-from passwordmanager import app
+from passwordmanager import app, db, bcrypt
 from passwordmanager.forms import RegistrationForm, LoginForm
 from passwordmanager.models import User, Account
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 
@@ -16,23 +17,53 @@ def about():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'გილოცავ {form.first_name.data}! თქვენ წარმატებით გაიარეთ რეგისტრაცია', 'flash_success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        flash(f'გილოცავ {form.first_name.data}! თქვენ წარმატებით გაიარეთ რეგისტრაცია', 'flash-success')
+        return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'test@gmail.com' and form.password.data == 'test':
-            flash(f'გილოცავთ ავტორიზაცია წარმატებით განხორციელდა', 'flash_success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('ავტორიზაცია წარმატებით განხორციელდა', 'flash-success')
+            return redirect(url_for('account'))
         else:
-            flash('unsuccsesful', 'flash_fail')
+            flash('ელ-ფოსტა ან პაროლი არასწორია , გთხოვთ ცადოთ თავიდან', 'flash-fail')
 
     return render_template('login.html', form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('თქვენ გამოხვედით სისტემიდან', 'flash-fail')
+    return redirect(url_for('login'))
+
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
 
