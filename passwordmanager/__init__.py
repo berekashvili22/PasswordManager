18211b9ec38965fd9ef47e8508799595167f5012
tornike <tornike.berekashvili.1@btu.edu.ge>
2020-10-12 17:23:18 +0400

    added login manager

--- passwordmanager/__init__.py
+++ passwordmanager/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a0e27327a62069c1f3f059bbb7605b09'
-app.config['SQLACHEMY_DATABE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app) 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app) 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'გთხოვთ გაიაროთ ავტორიზაცია რათა ეწვიოთ აღნიშნულ გვერდს'
+login_manager.login_message_category = 'flash-info'
 
 from passwordmanager import routes
