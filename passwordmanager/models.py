dc3daccc2a5f93202e32e778a72b16e759588754
tornike <tornike.berekashvili.1@btu.edu.ge>
2020-10-12 17:23:07 +0400

    added user_auth to models

--- passwordmanager/models.py
+++ passwordmanager/models.py
@@ -1,8 +1,14 @@
-from passwordmanager import db
+from passwordmanager import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
