ef940aeae589ffff13ecac524f90cc1d1de37b45
tornike <tornike.berekashvili.1@btu.edu.ge>
2020-10-15 23:35:01 +0400

    .

--- passwordmanager/static/main.js
+++ passwordmanager/static/main.js
@@ -1,11 +1,5 @@
-var modalBtn = document.querySelector('.add-password-btn');
-var modalBg = document.querySelector('.modal-bg');
-var modalClose = document.querySelector('.modal-close');
+             1
 
-modalBtn.addEventListener('click', function(){
-    modalBg.classList.add('modal-bg-active');
-});
 
-modalClose.addEventListener('click', function(){
-    modalBg.classList.remove('modal-bg-active');
-});
+//             2
+
