73c161c33e31905540eb3f7caf3b924a3c6233ff
tornike <tornike.berekashvili.1@btu.edu.ge>
2020-11-07 18:52:54 +0400

    created method for generating token for user

--- passwordmanager/models.py
+++ passwordmanager/models.py
@@ -1,6 +1,7 @@
-from passwordmanager import db, login_manager
+from passwordmanager import db, login_manager, app
 from datetime import datetime
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer # for password reset
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -16,6 +17,21 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     accounts = db.relationship('Account', backref='owner', lazy=True)
 
+   
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+        
     def __rerp__(self):
         return f"User('{self.first_name}', '{self.last_name}', '{self.email}')"
 
